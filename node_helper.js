28c2b0550fa79a10625d544d2ceb9a56f57e2946
John Wade <cowboysdude@stny.rr.com>
2017-06-21 21:56:53 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -7,7 +7,6 @@
 const NodeHelper = require('node_helper');
 const request = require('request');
 const parser = require('xml2js').parseString;
-const fs = require('fs');
 
 
 module.exports = NodeHelper.create({
@@ -23,12 +22,9 @@ module.exports = NodeHelper.create({
     	        }, (error, response, body) => {
             if (!error && response.statusCode === 200) {
                 parser(body, (err, result)=> {
-                    if(result.hasOwnProperty('xml')){
-                    //    var result = JSON.parse(JSON.stringify(result.xml.channel[0].item[0]));
 						var result = JSON.parse(JSON.stringify(result));
-						console.log(result + response.statusCode);
                         this.sendSocketNotification("EARTHQUAKE_RESULT", result);
-                    }
+                   
                 });
             }
        });
